7f7cd0809d5ac7f508670276a38143d1a22ada6e
Bitjockey <bitjockey@jackpoint.network>
2022-05-07 15:14:37 -0400

    Method to prepare data

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from typing import Union
+from urllib import response
 
 import pandas as pd
 
@@ -9,18 +10,45 @@ def load_csv(filename):
     return pd.read_csv(filename)
 
 
-def prepare_context(
-    data_or_filename: Union[str, pd.DataFrame], character, content_key="content"
+def prepare_data(
+    data_or_filename: Union[str, pd.DataFrame],
+    filter_by: str = None,
+    filter_value: str = None,
+    content_key: str = "content",
+    n: int = 7,
+    test_size: float = 0.1,
 ):
     if isinstance(str):
         data = load_csv(data_or_filename)
 
-    character_indexes = data.loc[data["character"] == character].index
+    contexted_data = prepare_context(
+        data,
+        filter_by=filter_by,
+        filter_value=filter_value,
+        content_key=content_key,
+        n=n,
+    )
+
+    trn_df, val_df = train_test_split(contexted_data, test_size=test_size)
+
+    return {"train": trn_df, "validation": val_df}
+
+
+def prepare_context(
+    data: pd.DataFrame,
+    filter_by: str = None,
+    filter_value: str = None,
+    content_key="content",
+    n: int = 7,
+):
+    if filter_by:
+        indexes = data.loc[data[filter_by] == filter_value].index
+    else:
+        indexes = range(n, len(data[content_key]))
 
     contexted = []
-    n = 7
 
-    for i in character_indexes:
+    for i in indexes:
         row = []
         prev = i - 1 - n
         for j in range(i, prev, -1):
