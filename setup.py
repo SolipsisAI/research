45fca7b93f857f8202878aabf5c87c69fe3013bd
bitjockey <bitjockey@ajulian.me>
2022-05-18 23:25:26 +0000

    Create console scripts for training and chatting

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='src',
-    version='0.1.0',
-    packages=find_packages(include=['src.*'])
+    name="src",
+    version="0.1.0",
+    packages=find_packages(include=["src.*"]),
+    entry_points={
+        "console_scripts": [
+            "trainer=src.run:main",
+            "chat-test=src.chat:main",
+        ],
+    },
 )
