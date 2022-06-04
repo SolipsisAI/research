729e24c2f285d2cfecb46643a0c4b2836270cbbc
Bitjockey <git@ajulian.me>
2022-06-04 23:06:11 +0000

    Filter data by character

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,27 @@
-from typing import Union
+from typing import Union, Dict
 
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
 
-def prepare_data(data: Union[pd.DataFrame, str], text_key="content"):
+def prepare_data(
+    data: Union[pd.DataFrame, str], text_key="content", filter_args: Dict = None
+):
     """Prepare and split data into training and evaluation sets"""
     if isinstance(data, str):
         data = pd.read_csv(data)
+
+    if filter_args:
+        indexes = data.loc[data[filter_args["key"]] == filter_args["value"]].index
+    else:
+        indexes = range(n, len(data[text_key]))
 
     contexted = []
 
     n = 7
 
-    for i in range(n, len(data[text_key])):
+    for i in indexes:
         row = []
         prev = i - 1 - n
         for j in range(i, prev, -1):
