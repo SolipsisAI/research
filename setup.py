767203746a95e0d270109adffb1927e561c1072f
Bitjockey <bitjockey@jacktpoint.network>
2022-06-11 17:17:28 +0000

    Load requirements from files

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
+
+ROOT_DIR = Path().absolute()
+REQUIREMENTS_DIR = ROOT_DIR.joinpath("requirements")
+
+
+def read_requirements_from_dir(requirements_dir):
+    requirement_files = list(map(open, Path(requirements_dir).glob("*.txt")))
+    requirement_groups = list(
+        map(
+            lambda g: list(map(lambda i: i.strip(), g)),
+            map(lambda f: f.readlines(), requirement_files),
+        )
+    )
+    requirements = [
+        item for requirement_group in requirement_groups for item in requirement_group
+    ]
+    return requirements
+
 
 setup(
     name="src",
@@ -7,4 +27,5 @@ setup(
     entry_points={
         "console_scripts": ["solipsis-trainer=src.main:main"],
     },
+    install_requires=read_requirements_from_dir(REQUIREMENTS_DIR),
 )
