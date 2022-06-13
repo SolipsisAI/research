3ce7c8c12015a15b4b2e43394b806a57df62e703
Bitjockey <bitjockey@jacktpoint.network>
2022-06-13 08:58:22 +0000

    Try loading config

--- src/chat.py
+++ src/chat.py
@@ -2,7 +2,14 @@ import argparse
 import re
 
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, Conversation, pipeline
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    AutoConfig,
+    Conversation,
+    pipeline,
+    ConversationalPipeline,
+)
 
 from src.classifier import Classifier
 from src.utils import PAD_TOKEN
@@ -29,9 +36,14 @@ def generate_responses(model, tokenizer, text, chat_history_ids=None, step=0):
         no_repeat_ngram_size=3,
         do_sample=True,
         top_k=100,
-        top_p=0.7,
-        # temperature=0.8,
+        top_p=0.9,
+        temperature=0.7,
     )
+    # chat_history_ids = model.generate(
+    #     bot_input_ids,
+    #     max_length=1024,
+    #     pad_token_id=tokenizer.pad_token_id,
+    # )
 
     response = tokenizer.decode(
         chat_history_ids[:, bot_input_ids.shape[-1] :][0],
@@ -41,8 +53,12 @@ def generate_responses(model, tokenizer, text, chat_history_ids=None, step=0):
     return response, chat_history_ids, step + 1
 
 
-def chat(model, tokenizer, classifier=None):
+def chat(model, tokenizer, classifier=None, device=None):
     """Use model.generate to interact"""
+    model.config.pad_token_id = tokenizer.pad_token_id
+
+    model.to(device)
+
     step = 0
     chat_history_ids = []
 
@@ -64,22 +80,50 @@ def chat(model, tokenizer, classifier=None):
         print(f"Bot: {clean_text(response)}")
 
 
-def chat_pipeline(model, tokenizer, classifier=None):
-    """Use conversational pipeline to interact"""
-    pipe = pipeline("conversational", model=model, tokenizer=tokenizer)
-    # Disable the "Setting pad_token_id" message
-    # https://github.com/huggingface/transformers/issues/12020#issuecomment-898899723
-    pipe.model.config.pad_token_id = pipe.model.config.eos_token_id
+# def chat_pipeline2(model, tokenizer, classifier=None, device=None):
+#     """Use conversational pipeline to interact"""
+#     pipe = pipeline(
+#         "conversational",
+#         model=model,
+#         tokenizer=tokenizer,
+#         min_length_for_response=20,
+#         device=device,
+#     )
+#     # Disable the "Setting pad_token_id" message
+#     # https://github.com/huggingface/transformers/issues/12020#issuecomment-898899723
+#     pipe.model.config.pad_token_id = pipe.tokenizer.pad_token_id
 
-    conversation = None
+#     conversation = None
+
+#     while True:
+#         text = input(">> ")
+#         if text in ["/q", "/quit", "/e", "/exit"]:
+#             break
+
+#         if not conversation:
+#             conversation = Conversation()
+
+#         conversation.add_user_input(preprocess_text(text, classifier=classifier))
+
+#         print(f"User: {text}")
+
+#         result = pipe(conversation)
+#         response = result.generated_responses[-1]
+
+#         print(f"Bot: {clean_text(response)}")
+
+
+def chat_pipeline(model, tokenizer, classifier=None, device=None):
+    conversation = Conversation()
+    pipe = ConversationalPipeline(
+        model=model, tokenizer=tokenizer, device=-1 if device == "cpu" else device
+    )
+    pipe.model.config.pad_token_id = pipe.tokenizer.pad_token_id
 
     while True:
         text = input(">> ")
         if text in ["/q", "/quit", "/e", "/exit"]:
             break
-
-        if not conversation:
-            conversation = Conversation()
 
         conversation.add_user_input(preprocess_text(text, classifier=classifier))
 
@@ -110,8 +154,16 @@ def main():
 
     parser.add_argument("--model_name", "-m")
     parser.add_argument("--tokenizer", "-t")
-    parser.add_argument("--classifier", "-c", default=None)
+    parser.add_argument("--config", "-c")
+    parser.add_argument("--classifier", "-cf", default=None)
     parser.add_argument("--pipeline", "-p", action="store_true", default=False)
+    parser.add_argument(
+        "--device",
+        "-d",
+        type=str,
+        default="cuda" if torch.cuda.is_available() else "cpu",
+        help="Device (cuda or cpu)",
+    )
 
     args = parser.parse_args()
 
@@ -120,8 +172,28 @@ def main():
     if args.classifier is not None:
         classifier = Classifier(model=args.classifier)
 
-    finetuned_model = AutoModelForCausalLM.from_pretrained(args.model_name)
-    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer, pad_token=PAD_TOKEN)
+    if not args.config:
+        args.config = args.model_name
+
+    config = AutoConfig.from_pretrained(args.config)
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_name,
+        from_tf=False,
+        config=config,
+    )
+
+    config.min_length = 2
+    config.max_length = 1000
+
+    print(f"min_length: {config.min_length}")
+    print(f"max_length: {config.max_length}")
+
+    # https://github.com/huggingface/transformers/issues/7800#issuecomment-709021207
+    # config = AutoConfig.from_pretrained(args.model_name)
+    # model = AutoModelForCausalLM.from_pretrained(args.model_name)
+    # model.to(args.device)
+    # tokenizer = AutoTokenizer.from_pretrained(args.tokenizer, pad_token=PAD_TOKEN)
     chat_fn = chat_pipeline if args.pipeline else chat
 
-    chat_fn(finetuned_model, tokenizer, classifier=classifier)
+    chat_fn(model, tokenizer, classifier=classifier, device=args.device)
