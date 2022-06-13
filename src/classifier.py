bc7d783ae91844aa8119f0799d006441c03443cf
Bitjockey <bitjockey@jacktpoint.network>
2022-06-13 02:49:08 +0000

    Make Classifier more robust by returning top k elements

--- src/classifier.py
+++ src/classifier.py
@@ -1,20 +1,40 @@
-from typing import Union
+from typing import Union, List, Dict
 
 from transformers import PreTrainedModel, pipeline
 
 from src.utils import DATA_DIR, read_json
 
-ID2LABEL_FILE = DATA_DIR.joinpath("id2label.json")
-LABEL2ID_FILE = DATA_DIR.joinpath("label2id.json")
+ID2LABEL_FILEPATH = DATA_DIR.joinpath("id2label.json")
+LABEL2ID_FILEPATH = DATA_DIR.joinpath("label2id.json")
 
 
 class Classifier:
-    def __init__(self, model: Union[str, PreTrainedModel]) -> None:
+    def __init__(
+        self, model: Union[str, PreTrainedModel], id2label_file=None, label2id_file=None
+    ) -> None:
+        if not id2label_file:
+            id2label_file = ID2LABEL_FILEPATH
+        if not label2id_file:
+            label2id_file = LABEL2ID_FILEPATH
         self.pipe = pipeline("text-classification", model=model, return_all_scores=True)
-        self.pipe.model.config.id2label = read_json(ID2LABEL_FILE)
-        self.pipe.model.config.label2id = read_json(LABEL2ID_FILE)
+        self.pipe.model.config.id2label = read_json(id2label_file)
+        self.pipe.model.config.label2id = read_json(label2id_file)
 
-    def classify(self, text):
+    def classify(self, text, k: int = 1) -> Union[List[Dict], List[str]]:
         results = self.pipe(text)
-        max_score = max(results[0], key=lambda x: x["score"])
-        return max_score["label"]
+        result = results[0] if results else None
+
+        if not result:
+            return
+
+        result.sort(key=lambda item: item.get("score"), reverse=True)
+
+        # Return all the results
+        if k < 1:
+            return result
+
+        # Sort by score, in descending order
+        result.sort(key=lambda item: item.get("score"), reverse=True)
+
+        # Return the top k results
+        return [r["label"] for r in result[:k]]
