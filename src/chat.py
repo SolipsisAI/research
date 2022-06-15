ce912cda3ec8e07aef4e5cf1a66f324490098ac1
Bitjockey <bitjockey@jacktpoint.network>
2022-06-15 07:47:17 +0000

    log chats

--- src/chat.py
+++ src/chat.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import argparse
 import re
 
@@ -16,52 +18,60 @@ from src.classifier import Classifier
 def chat(model, tokenizer, device, classifier=None, max_length: int = 1000):
     """Use model.generate to interact"""
     model.config.pad_token_id = tokenizer.pad_token_id
-
     model.to(device)
 
-    step = 0
+    with open(f"chatlog-{datetime.now().isoformat()}.txt", "w+") as chatlog:
+        step = 0
+        while True:
+            text = input(">> ")
 
-    while True:
-        text = input(">> ")
-        if text in ["/q", "/quit", "/e", "/exit"]:
-            break
+            if not text:
+                continue
 
-        print(f"User: {text}")
+            if text in ["/q", "/quit", "/e", "/exit"]:
+                break
 
-        new_user_input_ids = tokenizer.encode(
-            preprocess_text(text, classifier=classifier) + tokenizer.eos_token,
-            return_tensors="pt",
-        )
+            print(f"User: {text}")
 
-        # append the new user input tokens to the chat history
-        bot_input_ids = (
-            torch.cat([chat_history_ids, new_user_input_ids], dim=-1)
-            if step > 0
-            else new_user_input_ids
-        )
+            text = preprocess_text(text, classifier=classifier)
+            chatlog.write(text + "\n")
 
-        # generate chat ids
-        chat_history_ids = model.generate(
-            bot_input_ids,
-            max_length=max_length,
-            pad_token_id=tokenizer.eos_token_id,
-            no_repeat_ngram_size=3,
-            do_sample=True,
-            top_k=100,
-            top_p=0.7,
-            temperature=0.8,
-        )
+            new_user_input_ids = tokenizer.encode(
+                text + tokenizer.eos_token,
+                return_tensors="pt",
+            )
 
-        response = tokenizer.decode(
-            chat_history_ids[:, bot_input_ids.shape[-1] :][0],
-            skip_special_tokens=True,
-        )
+            # append the new user input tokens to the chat history
+            bot_input_ids = (
+                torch.cat([chat_history_ids, new_user_input_ids], dim=-1)
+                if step > 0
+                else new_user_input_ids
+            )
 
-        print(f"Bot: {postprocess_text(response)}")
+            # generate chat ids
+            chat_history_ids = model.generate(
+                bot_input_ids,
+                max_length=max_length,
+                pad_token_id=tokenizer.eos_token_id,
+                no_repeat_ngram_size=3,
+                do_sample=True,
+                top_k=100,
+                top_p=0.7,
+                temperature=0.8,
+            )
+
+            response = tokenizer.decode(
+                chat_history_ids[:, bot_input_ids.shape[-1] :][0],
+                skip_special_tokens=True,
+            )
+
+            response = postprocess_text(response)
+            chatlog.write(response + "\n")
+
+            print(f"Bot: {response}")
 
 
 def chat_pipeline(model, tokenizer, classifier=None, device=None, max_length=1000):
-    conversation = Conversation()
     pipe = ConversationalPipeline(
         model=model,
         tokenizer=tokenizer,
@@ -83,7 +93,7 @@ def chat_pipeline(model, tokenizer, classifier=None, device=None, max_length=100
         if text in ["/q", "/quit", "/e", "/exit"]:
             break
 
-        conversation.add_user_input(preprocess_text(text, classifier=classifier))
+        conversation = Conversation(preprocess_text(text, classifier=classifier))
 
         print(f"User: {text}")
 
@@ -104,7 +114,7 @@ def preprocess_text(text, classifier=None):
 
 def postprocess_text(text):
     """Clean response text"""
-    text = re.sub(r"^\w+", "", text)
+    text = re.sub(r"^\w+\s", "", text)
     return re.sub(r"_comma_", ",", text)
 
 
@@ -146,4 +156,10 @@ def main():
 
     chat_fn = chat_pipeline if args.pipeline else chat
 
-    chat_fn(model, tokenizer, classifier=classifier, device=args.device, max_length=args.max_length)
+    chat_fn(
+        model,
+        tokenizer,
+        classifier=classifier,
+        device=args.device,
+        max_length=args.max_length,
+    )
